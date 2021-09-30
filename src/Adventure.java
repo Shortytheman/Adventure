bb1202e708cbac58571caf641ad41d2f456eb746
Shortytheman <shor7ty@gmail.com>
2021-09-30 15:58:43 +0200

    Små rettelser

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Adventure {
@@ -8,17 +7,23 @@ public class Adventure {
   public String choice(){
     Scanner scanner = new Scanner(System.in);
     String choice;
-    do {
-    System.out.println("Wrong choice.. Plz input w, e, s or n for directions.");
+    System.out.print("Enter your direction, w for west, e for east, n for north and s for south: ");
     choice = scanner.nextLine();
-}
-while (!Objects.equals(choice, "w") || !Objects.equals(choice, "e") || !Objects.equals(choice, "s") || !Objects.equals(choice, "n"));
-    return choice;
+
+    if (choice.equalsIgnoreCase("w") || choice.equalsIgnoreCase("e") || choice.equalsIgnoreCase("s") || choice.equalsIgnoreCase("n")) {
+      return choice;
+    } else
+      do {
+        System.out.println("Wrong choice.. Plz input w, e, s or n for directions.");
+        choice = scanner.nextLine();
+      }
+      while ((!choice.equalsIgnoreCase("w") && !choice.equalsIgnoreCase("e") && !choice.equalsIgnoreCase("s") && !choice.equalsIgnoreCase("n")));
+      return choice;
   }
 
     public static void main (String[] args) {
-    Adventure gg = new Adventure();
-    gg.choice();
+    Adventure run = new Adventure();
+    run.choice();
     }
   }
 
