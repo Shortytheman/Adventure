83e1c8e4ae9088bf8f192168389210239e84a3ff
Shortytheman <shor7ty@gmail.com>
2021-09-30 19:51:13 +0200

    Choice færdig - + Merlin

--- src/Adventure.java
+++ src/Adventure.java
@@ -9,7 +9,7 @@ public class Adventure {
     String choice;
     System.out.println("Make your choice\n________________________ \nDirections: go north, go south, go west and go east to travel in the desired direction.\nAsk for help: help\nTo look around: look");
     choice = scanner.nextLine();
-    if (choice.equalsIgnoreCase("w") || choice.equalsIgnoreCase("e") || choice.equalsIgnoreCase("s") || choice.equalsIgnoreCase("n")) {
+    if (choice.equalsIgnoreCase("go west") || choice.equalsIgnoreCase("go east") || choice.equalsIgnoreCase("go south") || choice.equalsIgnoreCase("go north")) {
       return choice;
     }else if(choice.equalsIgnoreCase("exit")) {
       exitGame();
@@ -18,7 +18,7 @@ public class Adventure {
       help();
     }
     else if (choice.equalsIgnoreCase("look")){
-      look(currentRoom);
+      look();
     }
       do {
         System.out.println("Please enter one of the valid options");
@@ -27,17 +27,26 @@ public class Adventure {
           exitGame();
         } else if (choice.equalsIgnoreCase("help")) {
           help();
+        } else if (choice.equalsIgnoreCase("look")){
+          look();
         }
       }
-      while ((!choice.equalsIgnoreCase("w") && !choice.equalsIgnoreCase("e") && !choice.equalsIgnoreCase("s") && !choice.equalsIgnoreCase("n")));
+      while ((!choice.equalsIgnoreCase("go west") && !choice.equalsIgnoreCase("go east") && !choice.equalsIgnoreCase("go south") && !choice.equalsIgnoreCase("go north")));
       return choice;
   }
-  void look(Room room){
-    this.currentRoom = room;
-    System.out.println();
+
+  public Room getCurrentRoom(){
+    return this.currentRoom;
+  }
+  public void setCurrentRoom(Room room){
+  this.currentRoom = room;
+  }
+
+  void look(){
+    System.out.println(currentRoom);
   }
   void help(){
-    System.out.println("\n\n( ಠ ͜ʖ ಠ ) Hello outcast it is i Merlin, the great wizard. You've asked for advice on your journey and i shall provide you with your options.\n\n( ಠ ͜ʖ ಠ )⊃══⛧⌒｡ ~ALAKAZAM~");
+    System.out.println("\n\n( ಠ ͜ʖ ಠ ) Hello outcast it is I  --  Merlin, the great wizard. You've asked for advice on your journey and i shall provide you with your options.\n\n( ಠ ͜ʖ ಠ )⊃══⛧⌒｡ ~ALAKAZAM~");
     System.out.println("________________________\nDirections \nTo go north: \"go north\" \nTo go south: \"go south\" \nTo go west: \"go west\" \nTo go east: \"go east\"");
     System.out.println("________________________\nTo look around: \"Look\"");
     System.out.println("________________________\nTo exit the game: \"Exit\"\n________________________");
